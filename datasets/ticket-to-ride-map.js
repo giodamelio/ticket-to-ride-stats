a286d33c24ffca892adaa3f3b7102ff25bd2fbcc
Gio d'Amelio <giodamelio@gmail.com>
2014-10-26 03:17:55 -0700

    Add city nodes

--- datasets/ticket-to-ride-map.js
+++ datasets/ticket-to-ride-map.js
@@ -2,12 +2,237 @@ window.ticketToRideMap =  {
     nodes: [
         {
             data: {
-                id: "1"
+                id: "Edinburge"
             }
         },
         {
             data: {
-                id: "2"
+                id: "London"
+            }
+        },
+        {
+            data: {
+                id: "Dieppe"
+            }
+        },
+        {
+            data: {
+                id: "Brest"
+            }
+        },
+        {
+            data: {
+                id: "Lisboa"
+            }
+        },
+        {
+            data: {
+                id: "Madrid"
+            }
+        },
+        {
+            data: {
+                id: "Cadiz"
+            }
+        },
+        {
+            data: {
+                id: "Barcelona"
+            }
+        },
+        {
+            data: {
+                id: "Pamplona"
+            }
+        },
+        {
+            data: {
+                id: "Paris"
+            }
+        },
+        {
+            data: {
+                id: "Amsterdam"
+            }
+        },
+        {
+            data: {
+                id: "Bruxelles"
+            }
+        },
+        {
+            data: {
+                id: "Essen"
+            }
+        },
+        {
+            data: {
+                id: "Zurich"
+            }
+        },
+        {
+            data: {
+                id: "Marseille"
+            }
+        },
+        {
+            data: {
+                id: "Roma"
+            }
+        },
+        {
+            data: {
+                id: "Frankfurt"
+            }
+        },
+        {
+            data: {
+                id: "Munchen"
+            }
+        },
+        {
+            data: {
+                id: "Kobenhaven"
+            }
+        },
+        {
+            data: {
+                id: "Palermo"
+            }
+        },
+        {
+            data: {
+                id: "Brindisi"
+            }
+        },
+        {
+            data: {
+                id: "Venezia"
+            }
+        },
+        {
+            data: {
+                id: "Wien"
+            }
+        },
+        {
+            data: {
+                id: "Zagrab"
+            }
+        },
+        {
+            data: {
+                id: "Berlin"
+            }
+        },
+        {
+            data: {
+                id: "Stockholm"
+            }
+        },
+        {
+            data: {
+                id: "Budapest"
+            }
+        },
+        {
+            data: {
+                id: "Sarajevo"
+            }
+        },
+        {
+            data: {
+                id: "Athina"
+            }
+        },
+        {
+            data: {
+                id: "Smyrna"
+            }
+        },
+        {
+            data: {
+                id: "Angora"
+            }
+        },
+        {
+            data: {
+                id: "Erzurum"
+            }
+        },
+        {
+            data: {
+                id: "Constantinople"
+            }
+        },
+        {
+            data: {
+                id: "Bucuresti"
+            }
+        },
+        {
+            data: {
+                id: "Sochi"
+            }
+        },
+        {
+            data: {
+                id: "Sevastopol"
+            }
+        },
+        {
+            data: {
+                id: "Kharkov"
+            }
+        },
+        {
+            data: {
+                id: "Kyiv"
+            }
+        },
+        {
+            data: {
+                id: "Warszawa"
+            }
+        },
+        {
+            data: {
+                id: "Danzic"
+            }
+        },
+        {
+            data: {
+                id: "Rica"
+            }
+        },
+        {
+            data: {
+                id: "Wilno"
+            }
+        },
+        {
+            data: {
+                id: "Petrograd"
+            }
+        },
+        {
+            data: {
+                id: "Moskva"
+            }
+        },
+        {
+            data: {
+                id: "Rostov"
+            }
+        },
+        {
+            data: {
+                id: "Smolensk"
+            }
+        },
+        {
+            data: {
+                id: "Sofia"
             }
         }
     ],
