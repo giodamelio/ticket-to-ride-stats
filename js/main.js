059c4bfa03a2615095b122b5c11bfccdc9c5bdcc
Gio d'Amelio <giodamelio@gmail.com>
2014-10-26 02:15:21 -0700

    Enable panning and add select color

--- js/main.js
+++ js/main.js
@@ -36,11 +36,20 @@ $(document).ready(function() {
             .selector("node")
                 .css({
                     "content": "data(id)"
+                })
+            .selector("node:selected")
+                .css({
+                    "background-color": "#444444"
                 }),
 
         // Graph is ready
-        ready: function() {
+        ready: function(evt) {
+            var cy = evt.cy;
 
+            // Enable panning and disable box selection
+            cy.panningEnabled(true);
+            cy.userPanningEnabled(true);
+            cy.boxSelectionEnabled(false);
         }
     });
 });
